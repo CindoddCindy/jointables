18b67811ec3ebf8556097259a8447a08c470f8e5
CindoddCindy <cindodcindy@gmail.com>
2021-01-14 10:20:55 +0700

    tambah package

---
+++ tesjointables/src/main/java/tessjoinss/tesjointables/TesjointablesApplication.java
@@ -0,0 +1,13 @@
+package tessjoinss.tesjointables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesjointablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesjointablesApplication.class, args);
+	}
+
+}
---
+++ tesjointables/src/test/java/tessjoinss/tesjointables/TesjointablesApplicationTests.java
@@ -0,0 +1,13 @@
+package tessjoinss.tesjointables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesjointablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
