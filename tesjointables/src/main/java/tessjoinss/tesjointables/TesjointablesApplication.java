b0396ee6b561e6ade13ac084c7f16cadaa7dbc7b
CindoddCindy <cindodcindy@gmail.com>
2021-01-14 10:41:54 +0700

    tambah kelas kelas nya

--- tesjointables/src/main/java/tessjoinss/tesjointables/TesjointablesApplication.java
+++ tesjointables/src/main/java/tessjoinss/tesjointables/TesjointablesApplication.java
@@ -2,8 +2,10 @@ package tessjoinss.tesjointables;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TesjointablesApplication {
 
 	public static void main(String[] args) {
