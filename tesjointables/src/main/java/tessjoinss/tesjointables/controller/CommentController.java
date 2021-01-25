78acb70e3c0b770281d151ba118f42463d9d6584
CindoddCindy <cindodcindy@gmail.com>
2021-01-25 14:54:27 +0700

    tmabh upload image tapi masih salah

--- tesjointables/src/main/java/tessjoinss/tesjointables/controller/CommentController.java
+++ tesjointables/src/main/java/tessjoinss/tesjointables/controller/CommentController.java
@@ -4,12 +4,15 @@ package tessjoinss.tesjointables.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import tessjoinss.tesjointables.exception.ResourceNotFoundException;
 import tessjoinss.tesjointables.model.Comment;
 import tessjoinss.tesjointables.repository.CommentRepository;
 import tessjoinss.tesjointables.repository.PostRepository;
+import tessjoinss.tesjointables.service.FileStorageService;
 
 import javax.validation.Valid;
 
@@ -21,6 +24,10 @@ public class CommentController {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private FileStorageService storageService;
+
+
     @GetMapping("/posts/{postId}/comments")
     public Page<Comment> getAllCommentsByPostId(@PathVariable (value = "postId") Long postId,
                                                 Pageable pageable) {
@@ -28,13 +35,41 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public Comment createComment(@PathVariable (value = "postId") Long postId,
+    public Comment createComment(@PathVariable (value = "postId") Long postId,@RequestParam("file") MultipartFile file,
                                  @Valid @RequestBody Comment comment) {
+        try{
+            storageService.store(file);
+
+
+        }catch (Exception e){
+            }
         return postRepository.findById(postId).map(post -> {
             comment.setPost(post);
             return commentRepository.save(comment);
         }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+
+
     }
+
+/*
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+        try {
+            storageService.store(file);
+
+            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
+
+ */
+
+
+
 
     @PutMapping("/posts/{postId}/comments/{commentId}")
     public Comment updateComment(@PathVariable (value = "postId") Long postId,
